b6c1e014bdcecf7025becb837a47ee6cdc3ed2e3
nateh23 <neh45@byu.edu>
2025-02-07 10:20:46 -0700

    2/7/25 topbarbottom

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './app.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
-    return <div className='body bg-dark text-light'>App will display here</div>;
+    return (
+        <div>
+            <header>
+                <div>
+                    <h1 className="fancyTitle">Rock Paper Snail</h1>
+                    <span className="playerName text-muted">Username</span>
+                </div>
+                <nav className="navbar">
+                    <menu className="container-fluid">
+                        <li className="nav-item">
+                            <a className="nav-link text-muted" href="index.html">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link text-muted" href="play.html">Play</a>
+                        </li>
+                    </menu>
+                </nav>
+                <hr />
+            </header>
+
+            <footer>
+                <a className = "fancyTitle text-reset fixed-bottom" href="https://github.com/nateh23/startup">GitHub</a>
+            </footer>
+        </div>
+    );
 }
