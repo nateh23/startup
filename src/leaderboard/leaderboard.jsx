629113e1a68c62b6aa624690f1fdca14369f5a7f
nateh23 <neh45@byu.edu>
2025-02-26 18:06:09 -0700

    leaderboard

--- src/leaderboard/leaderboard.jsx
+++ src/leaderboard/leaderboard.jsx
@@ -1,34 +1,48 @@
 import React from 'react';
-
 import './leaderboard.css';
 
 export function Leaderboard() {
+    const [scores, setScores] = React.useState([]);
+
+    React.useEffect(() => {
+        const scoresText = localStorage.getItem('scores');
+        if (scoresText) {
+            setScores(JSON.parse(scoresText));
+        }
+    }, []);
+
+    const scoreRows = [];
+    if (scores.length) {
+        for (const [i, score] of scores.entries()) {
+            scoreRows.push(
+                <tr className="leaderBoardUser" key={i}>
+                    <td className="leaderSlot">{i + 1}</td>
+                    <td className="leaderSlot">{score.name.split('@')[0]}</td>
+                    <td className="leaderSlot">{score.score}</td>
+                </tr>
+            );
+        }
+    } else {
+        scoreRows.push(
+            <tr key='0'>
+                <td colSpan='4'>literally the snail is unbeatable</td>
+            </tr>
+        );
+    }
+
     return (
         <main>
             <table className="text-muted">
                 <thead>
-                    <th>
-                        Place
-                    </th>
-                    <th>
-                        User
-                    </th>
-                    <th>
-                        Score
-                    </th>
+                    <tr>
+                        <th>Place</th>
+                        <th>User</th>
+                        <th>Score</th>
+                    </tr>
                 </thead>
-
-                <tr className="leaderBoardUser">
-                    <td className = "leaderSlot">1st</td>
-                    <td className = "leaderSlot">Not the Snail</td>
-                    <td className = "leaderSlot">50000</td>
-                </tr>
-
-                <tr className="leaderBoardUser">
-                    <td className = "leaderSlot">2nd</td>
-                    <td className = "leaderSlot">Gary</td>
-                    <td className = "leaderSlot">49999</td>
-                </tr>
+                <tbody>
+                    {scoreRows}
+                </tbody>
             </table>
         </main>
     );
