c5900b9395da8060e9632ca2fe85eef3fc204d32
nateh23 <neh45@byu.edu>
2025-03-12 17:39:14 -0600

    index changes

--- service/index.js
+++ service/index.js
@@ -6,38 +6,131 @@ const app = express();
 
 const authCookieName = "token";
 
-//mines honestly so similar to simon haha
+//mines system is so similar to simon this is gonna look pretty much the same
+//since like why reinvent the wheel y'know
 let users = [];
 let scores = [];
 
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
-//init stack
 app.use(express.json());
 app.use(cookieParser());
 
-//prep public
 app.use(express.static('public'));
 
-//route this
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
-//create user
-//login user
-//delete user
+apiRouter.post('/auth/create', async (req, res) => {
+if (await findUser('email', req.body.email)) {
+    res.status(409).send({ msg: 'Existing user' });
+} else {
+    const user = await createUser(req.body.email, req.body.password);
+
+    setAuthCookie(res, user.token);
+    res.send({ email: user.email });
+}
+});
+
+apiRouter.post('/auth/login', async (req, res) => {
+    const user = await findUser('email', req.body.email);
+    if (user) {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+        user.token = uuid.v4();
+        setAuthCookie(res, user.token);
+        res.send({ email: user.email });
+        return;
+        }
+    }
+    res.status(401).send({ msg: 'Unauthorized' });
+});
+
+apiRouter.delete('/auth/logout', async (req, res) => {
+    const user = await findUser('token', req.cookies[authCookieName]);
+    if (user) {
+        delete user.token;
+    }
+    res.clearCookie(authCookieName);
+    res.status(204).end();
+});
 
 //verification to make sure user can submit score
+const verifyAuth = async (req, res, next) => {
+    const user = await findUser('token', req.cookies[authCookieName]);
+    if (user) {
+        next();
+    } else {
+        res.status(401).send({ msg: 'Unauthorized' });
+    }
+};
 
-//get all scores
 //submit the users score
+apiRouter.get('/scores', verifyAuth, (_req, res) => {
+    res.send(scores);
+});
 
+apiRouter.post('/score', verifyAuth, (req, res) => {
+    scores = updateScores(req.body);
+    res.send(scores);
+});
 
-//error handler
-//home page if theres a failure
+app.use(function (err, req, res, next) {
+    res.status(500).send({ type: err.name, message: err.message });
+});
 
+app.use((_req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+});
 
-//low level funcs
+function updateScores(newScore) {
+    let found = false;
+    for (const [i, prevScore] of scores.entries()) {
+        if (newScore.score > prevScore.score) {
+        scores.splice(i, 0, newScore);
+        found = true;
+        break;
+        }
+    }
 
+    if (!found) {
+        scores.push(newScore);
+    }
+
+    if (scores.length > 10) {
+        scores.length = 10;
+    }
+
+    return scores;
+}
+
+async function createUser(email, password) {
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = {
+        email: email,
+        password: passwordHash,
+        token: uuid.v4(),
+    };
+    users.push(user);
+
+    return user;
+}
+
+async function findUser(field, value) {
+    if (!value) return null;
+
+    return users.find((u) => u[field] === value);
+}
+
+function setAuthCookie(res, authToken) {
+    res.cookie(authCookieName, authToken, {
+        secure: true,
+        httpOnly: true,
+        sameSite: 'strict',
+    });
+}
 
 //debug listener
+app.listen(port, () => {
+console.log(`Listening on port ${port}`);
+});
