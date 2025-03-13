95ed6b5ad2165820e521943e0ca45ba589ec0096
nateh23 <neh45@byu.edu>
2025-03-12 22:03:59 -0600

    port config for publish

--- service/index.js
+++ service/index.js
@@ -11,7 +11,7 @@ const authCookieName = "token";
 let users = [];
 let scores = [];
 
-const port = process.argv.length > 2 ? process.argv[2] : 3000;
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 app.use(express.json());
 app.use(cookieParser());
