98644d72a51f6dbd48075b45b1ea42fb051b7f04
nateh23 <neh45@byu.edu>
2025-03-27 10:20:55 -0600

    added to server

--- service/index.js
+++ service/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const uuid = require('uuid');
 const app = express();
 const dataBase = require('./database.js');
+const { peerProxy } = require('./peerProxy.js');
 
 const authCookieName = "token";
 
@@ -140,6 +141,8 @@ function setAuthCookie(res, authToken) {
 }
 
 //debug listener
-app.listen(port, () => {
-console.log(`Listening on port ${port}`);
+const httpService = app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });
+
+peerProxy(httpService);
